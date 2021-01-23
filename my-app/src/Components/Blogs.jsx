af3499511934366261f43f99e6ae0d2c666d0148
Paras Bansal <parasbansal685@gmail.com>
2021-01-23 17:36:30 +0530

    Cards Added

--- my-app/src/Components/Blogs.jsx
+++ my-app/src/Components/Blogs.jsx
@@ -1,8 +1,12 @@
-import React, {useEffect} from "react";
+import React, {useEffect, useState} from "react";
 
 import axios from "axios";
 
+import Card from "react-bootstrap/Card";
+
 function Blogs(){
+
+    const [blogs, setBlogs] = useState([]);
     
     useEffect(() => {
         const dataFetch = async() => {
@@ -11,8 +15,10 @@ function Blogs(){
                     "Access-Control-Allow-Origin": "*"
                 }
                 const res = await axios.get("https://mostlypandatuts.herokuapp.com/api/blogs", headers);
-                console.log(res);
-
+                console.log(res.data);
+                if(res.data){
+                    setBlogs(res.data);
+                }
             }
             catch(error){
                 console.log(error);
@@ -26,7 +32,39 @@ function Blogs(){
 
     return (
         <> 
-            
+            {blogs ? (
+                <>
+                    {blogs.map((blog) => (
+                        <Card style={{ width: '18rem' }}>
+                            <Card.Body>
+                                <Card.Title>{blog.title}</Card.Title>
+                                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                                <Card.Text>
+                                Some quick example text to build on the card title and make up the bulk of
+                                the card's content.
+                                </Card.Text>
+                            
+                            </Card.Body>
+                        </Card>
+                    ))
+
+                    }
+                </>
+            ) : (
+                <></>
+            )
+            }
+            {/* <Card style={{ width: '18rem' }}>
+                <Card.Body>
+                    <Card.Title>Card Title</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+                    <Card.Text>
+                    Some quick example text to build on the card title and make up the bulk of
+                    the card's content.
+                    </Card.Text>
+                   
+                </Card.Body>
+            </Card> */}
         </>
     )
 }
