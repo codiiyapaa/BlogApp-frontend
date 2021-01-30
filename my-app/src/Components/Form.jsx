93d5370536d00b8de988c198daf857d214e15e11
Paras Bansal <parasbansal685@gmail.com>
2021-01-30 12:23:22 +0530

    some post api changes

--- my-app/src/Components/Form.jsx
+++ my-app/src/Components/Form.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import Form from 'react-bootstrap/Form';
 import Container from "react-bootstrap/esm/Container";
 import Row from "react-bootstrap/Row";
@@ -7,6 +7,29 @@ import Button from 'react-bootstrap/Button';
 //import BorderPage from "./Borders";
 
 function Forms(){
+    const [blogPost, setBLog] = useState({
+        title: '',
+        author: '',
+        desc: ''
+    });
+
+    const handleChange= (e)=>{
+        setBLog({
+            ...blogPost,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const onSubmit =(e)=>{
+        if(blogPost.title.trim()!=="" && blogPost.author.trim()!=="" && blogPost.desc.trim()!==""){
+
+        }else{
+            window.alert("One of the Fields is empty");
+        }
+    }
+
+
+
     return(
         <div style={{
             border: '2px solid black',
@@ -21,23 +44,23 @@ function Forms(){
             <Form.Row>
                 <Form.Group as={Col} controlId="Title">
                 <Form.Label column="lg" >Title</Form.Label>
-                <Form.Control size="lg" placeholder="Enter Title" />
+                <Form.Control className="input" name="title" value={blogPost.title} onChange={handleChange} size="lg" placeholder="Enter Title" />
                 </Form.Group>
 
                 <Form.Group as={Col} controlId="Author Name">
                 <Form.Label column="lg">Author Name</Form.Label>
-                <Form.Control size="lg" placeholder="Enter Author Name" />
+                <Form.Control className="input" name="title" value={blogPost.title} onChange={handleChange} size="lg" placeholder="Enter Author Name" />
                 </Form.Group>
             </Form.Row>
 
                 <Form.Group controlId="Description">
                 <Form.Label column="lg">Description</Form.Label>
-                <Form.Control as="textarea" rows={3} size="lg" placeholder="Enter description" />
+                <Form.Control className="input" name="title" value={blogPost.title} onChange={handleChange} as="textarea" rows={3} size="lg" placeholder="Enter description" />
                 </Form.Group>
 
                 <Form.Group as={Row}>
                     <Col sm={{ span: 4, offset: 8, Order: "first"}}>
-                    <Button type="submit" variant="dark" size="lg" block>Submit</Button>
+                    <Button type="submit" variant="dark" onClick={onSubmit}  size="lg" block>Submit</Button>
                     </Col>
                 </Form.Group>
         </Form>
--- my-app/src/Components/Home.jsx
+++ my-app/src/Components/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-
 import Blogs from "./Blogs";
 import Form from "./Form";
 import Container from "react-bootstrap/Container";
@@ -7,6 +6,16 @@ import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 //import BorderPage from "./Borders";
 //import Alert from "react-bootstrap/Alert";
+
+/*const ColoredLine = ({ color }) => (
+    <hr
+        style={{
+            color: color,
+            backgroundColor: color,
+            height: 2
+        }}
+    />
+);*/
 
 function Home () {
     return(
